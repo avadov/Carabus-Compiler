a7ab1c24bfd1129e8eb5f6de00b2acddc3bf5add
Konstantin Avadov <konstantin.avadov@yahoo.com>
2023-12-15 23:46:42 +0300

    Writing assembly code to file

--- skeleton.c
+++ skeleton.c
@@ -9,6 +9,12 @@
 // Lookahead letter of a program text
 char nextChar;
 
+// Program source
+FILE *src;
+
+// Output file for translation of the source code to assembly language
+FILE *asm_code;
+
 // Read next character from input
 void readChar() {
     nextChar = getc(stdin);
@@ -64,19 +70,71 @@ char getNum() {
 }
 
 // Print a string after tab
-void emit(const char * str) {
+void encode(const char * str) {
     printf("\t%s", str);
+    fprintf(asm_code, "\t%s", str);
 }
 
 // Print a string with tab and \n
-void emitLn(const char * str) {
+void encodeLn(const char * str) {
     printf("\t%s\n", str);
+    fprintf(asm_code, "\t%s\n", str);
+}
+
+// Parse and translate a single term
+void term() {
+    char num = getNum();
+    encode("MOVE $");
+    printf("%c", num);
+    encodeLn(", %DX");
+}
+
+// Translate an addition
+void add() {
+    match('+');
+    term();
+    encodeLn("ADD %AX, %DX");
+}
+
+// Translate a subtraction
+void subtract() {
+    match('-');
+    term();
+    encodeLn("SUB %DX, %AX");
+    encodeLn("MOV %AX, %DX");
+}
+
+// Parse and translate an expression
+void expression() {
+    term();
+    encodeLn("MOVE %DX, %AX");
+    switch (nextChar) {
+        case '+':
+            add();
+            break;
+        case '-':
+            subtract();
+            break;
+        default:
+            expected("Operation");
+    }
 }
 
 void init() {
+    asm_code = fopen("asm.s", "w");
     readChar();
+}
+
+// Compile assembly code to executable file
+void compile() {
+    fclose(asm_code);
 }
 
 int main() {
     init();
+    expression();
+
+    compile();
 }
+
+
